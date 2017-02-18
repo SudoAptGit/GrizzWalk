2b90b82a5040357645dca0c560f8f9c70428e72c
Matthew Schmidt <shmigle@hotmail.com>
2017-02-18 01:50:58 -0500

    Add files via upload

---
+++ app/app/build/generated/source/buildConfig/release/com/grizzzwalk/mjschmidt/grizzwalk/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.grizzzwalk.mjschmidt.grizzwalk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.grizzzwalk.mjschmidt.grizzwalk";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
